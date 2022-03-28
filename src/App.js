83d1916a511b81bb0a77940efb84f510b94ffee7
jisun Ko <rhwltjs00@naver.com>
2022-03-29 01:48:06 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+    let posts = '여기서 다 찾자!';
     return(
-      <div classmate="App">
-          <div classmate="nav">
+      <div className='App'>
+          <div className='nav'>
             <div>#LOGO#</div>
           </div>
-
+          <h3>{ posts }</h3>
       </div>
     );
 }
